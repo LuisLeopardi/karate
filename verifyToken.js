229a0f3ad9f0bab695797c4faefa06009cc4bef5
Luis Leopardi <supernano1998@gmail.com>
2020-07-19 03:43:50 -0400

    commit

--- verifyToken.js
+++ verifyToken.js
@@ -1,8 +1,6 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-dotenv.config()
-
 module.exports = function verify (req,res,next) {
     const token = req.header('auth-token')
     if(!token) return res.status('401').send('acceso denegado');
